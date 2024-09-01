7cf5d69b9e4d78747726a8d77bcf08693dbcd6c7
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-09-01 13:06:55 +0000

    homes_controller再作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   root 'homes#top'
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
