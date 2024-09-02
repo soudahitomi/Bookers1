a1f9244d772a65a6b424a4043ffa6a424057f34c
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-09-02 03:49:46 +0000

    index,

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
 class BooksController < ApplicationController
-  def create
-
+  def index
+    @book = Book.new
+    @books = Book.all
   end
 
-  def index
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def show
@@ -13,5 +17,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
