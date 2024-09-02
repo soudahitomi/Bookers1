6060a6d5d6202c273c9bb3f4ed2490f763b8831b
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-09-02 02:26:43 +0000

    booksのルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  resources :books
   root 'homes#top'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
