1a1010b1421b088b64e4f7605c5a3ff332c84c42
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-09-06 11:29:43 +0000

    フラッシュメッセージ完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,13 +7,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
     end
-
-    # Book was successfully created.
 
   end
 
@@ -23,12 +22,12 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    # Book was successfully updated.
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(book.id)
     else
       render :edit
@@ -38,8 +37,9 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
-    # Book was successfully destroyed.
+
   end
 
   private
